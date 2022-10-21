bc3c3fd6791a36c19383801496ab9b20ccf5eb28
Zachary Smith <zach2993@gmail.com>
2022-10-21 09:14:34 -0500

    initial commit

---
+++ Legacy/Controllers/UserProfilesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Legacy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfilesController : ControllerBase
+    {
+    }
+}
